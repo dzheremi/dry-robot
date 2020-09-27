bd56ef950af0c6d6167d4a731a553151adfdba99
Jeremy Mercer <dzheremi@outlook.com>
2020-09-27 18:53:54 +1000

    Added basic feature test

---
+++ spec/feature/dry_robot_spec.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+RSpec.describe 'DryRobot' do
+  context 'with valid commands' do
+    let(:commands) do
+      [
+        'PLACE 0,0,N',
+        'MOVE',
+        'MOVE',
+        'MOVE',
+        'RIGHT',
+        'MOVE',
+        'MOVE',
+      ]
+    end
+
+    before do
+      commands.each do |command|
+        AppContainer['commands.adapter'].call(command: command)
+      end
+    end
+
+    it 'keeps track of the robots movements' do
+      expect(AppContainer['commands.adapter'].call(command: 'REPORT')).to be_success
+      expect(AppContainer['commands.adapter'].call(command: 'REPORT').value!).to eq('3,2,E')
+    end
+  end
+
+  context 'with commands leading to a fall' do
+    let(:commands) do
+      [
+        'PLACE 0,0,N',
+        'MOVE',
+        'MOVE',
+        'MOVE',
+        'MOVE',
+      ]
+    end
+
+    before do
+      commands.each do |command|
+        AppContainer['commands.adapter'].call(command: command)
+      end
+    end
+
+    it 'prevents the robot falling off the table' do
+      expect(AppContainer['commands.adapter'].call(command: 'MOVE')).to be_failure
+    end
+  end
+end
