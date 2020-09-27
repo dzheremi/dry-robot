5cc1c38328f9c0d344a98ff59953f38ca54ae6e3
Jeremy Mercer <dzheremi@outlook.com>
2020-09-27 10:02:26 +1000

    Command types and parsing

--- lib/dry_robot/commands/types.rb
+++ lib/dry_robot/commands/types.rb
@@ -8,13 +8,15 @@ module DryRobot
     module Types
       include Dry.Types()
 
-      BASE_COMMANDS = %w[PLACE MOVE LEFT RIGHT REPORT].freeze
-
-      Command = Types.Constructor(Types::Strict::String) do |value|
+      Command = Types.Constructor(Types::Strict::Array) do |value|
         CommandParser.parse(value)
       end
 
-      BaseCommandArguments = Types::Strict::String.enum(*BASE_COMMANDS)
+      POSITION_COMMANDS = %w[PLACE].freeze
+      SINGLE_COMMANDS = %w[MOVE LEFT RIGHT REPORT].freeze
+
+      PositionCommandArguments = Types::Strict::String.enum(*POSITION_COMMANDS)
+      SingleCommandArguments = Types::Strict::String.enum(*SINGLE_COMMANDS)
       PositionArgument = Strict::String.constrained(
         format: /^[1-5]{1},[1-5]{1},[NSEW]{1}$/,
       )
@@ -27,7 +29,46 @@ module DryRobot
           result.value! unless result.failure?
         end
 
-        def call(input); end
+        def call(input)
+          arguments_from_string(input).bind do |arguments|
+            parse_single_command(arguments).bind { |command| return Success(command) }
+            parse_position_command(arguments).bind { |command| return Success(command) }
+          end
+
+          Failure(input)
+        end
+
+        private
+
+        def arguments_from_string(string)
+          arguments = string.split(' ')
+          if arguments.size <= 2
+            Success(arguments)
+          else
+            Failure(string)
+          end
+        end
+
+        def parse_single_command(arguments)
+          return Failure(arguments) if arguments.size > 1
+
+          Success([SingleCommandArguments[arguments[0]]])
+        rescue Dry::Types::ConstraintError
+          Failure(arguments[0])
+        end
+
+        def parse_position_command(arguments)
+          return Failure(arguments) unless arguments.size == 2
+
+          Success(
+            [
+              PositionCommandArguments[arguments[0]],
+              PositionArgument[arguments[1]],
+            ],
+          )
+        rescue Dry::Types::ConstraintError
+          Failure(arguments[0])
+        end
       end
     end
   end
--- spec/lib/dry_robot/commands/types_spec.rb
+++ spec/lib/dry_robot/commands/types_spec.rb
@@ -4,11 +4,11 @@ require 'spec_helper'
 require 'dry_robot/commands/types'
 
 RSpec.describe DryRobot::Commands::Types do
-  describe 'base command arguments' do
-    subject(:command_arguments) { DryRobot::Commands::Types::BaseCommandArguments }
+  describe 'single command arguments' do
+    subject(:command_arguments) { DryRobot::Commands::Types::SingleCommandArguments }
 
     context 'with valid commands' do
-      %w[PLACE MOVE LEFT RIGHT REPORT].each do |command|
+      %w[MOVE LEFT RIGHT REPORT].each do |command|
         it "accepts the #{command} command" do
           expect(command_arguments[command]).to eq(command)
         end
@@ -21,8 +21,27 @@ RSpec.describe DryRobot::Commands::Types do
       end
 
       it 'rejects malformed known commands' do
-        expect { command_arguments['MOVE '] }.to raise_error(Dry::Types::ConstraintError)
-        expect { command_arguments[' PLACE'] }.to raise_error(Dry::Types::ConstraintError)
+        expect { command_arguments['move please'] }.to raise_error(Dry::Types::ConstraintError)
+      end
+    end
+  end
+
+  describe 'position command arguments' do
+    subject(:command_arguments) { DryRobot::Commands::Types::PositionCommandArguments }
+
+    context 'with valid commands' do
+      it 'accepts the PLACE command' do
+        expect(command_arguments['PLACE']).to eq('PLACE')
+      end
+    end
+
+    context 'with invalid commands' do
+      it 'rejects an unknown command' do
+        expect { command_arguments['DANCE'] }.to raise_error(Dry::Types::ConstraintError)
+      end
+
+      it 'rejects malformed known commands' do
+        expect { command_arguments['place'] }.to raise_error(Dry::Types::ConstraintError)
       end
     end
   end
@@ -58,6 +77,76 @@ RSpec.describe DryRobot::Commands::Types do
       context 'with a completely malformed position' do
         it 'rejects the position' do
           expect { position_argument['The Rabit Hole'] }.to raise_error(Dry::Types::ConstraintError)
+        end
+      end
+    end
+  end
+
+  describe 'command parsing' do
+    subject(:command_parser) { DryRobot::Commands::Types::Command }
+
+    context 'with single commands' do
+      context 'with valid input' do
+        %w[MOVE LEFT RIGHT REPORT].each do |command|
+          it 'returns the command as an array' do
+            expect(command_parser[command]).to be_an(Array)
+          end
+
+          it 'returns an array with only one instruction' do
+            expect(command_parser[command].size).to eq(1)
+          end
+
+          it 'accepts the command' do
+            expect(command_parser[command]).to eq([command])
+          end
+        end
+      end
+
+      context 'with invalid input' do
+        it 'rejects unknown commands' do
+          expect { command_parser['DANCE'] }.to raise_error(Dry::Types::ConstraintError)
+        end
+
+        it 'rejects malformed commands' do
+          expect { command_parser['left'] }.to raise_error(Dry::Types::ConstraintError)
+        end
+      end
+    end
+
+    context 'with the position commands' do
+      context 'with valid input' do
+        let(:command) { 'PLACE 1,1,N' }
+
+        it 'returns the command as an array' do
+          expect(command_parser[command]).to be_an(Array)
+        end
+
+        it 'returns an array with only two instructions' do
+          expect(command_parser[command].size).to eq(2)
+        end
+
+        it 'accepts the command' do
+          expect(command_parser[command]).to eq(['PLACE', '1,1,N'])
+        end
+      end
+
+      context 'with invalid input' do
+        context 'with two unknown commands' do
+          it 'rejects the command' do
+            expect { command_parser['PERFORM BACKFLIP'] }.to raise_error(Dry::Types::ConstraintError)
+          end
+        end
+
+        context 'with a malformed position' do
+          it 'rejects the command' do
+            expect { command_parser['PLACE 1232,N'] }.to raise_error(Dry::Types::ConstraintError)
+          end
+        end
+
+        context 'with a position which is out of bounds' do
+          it 'rejects the command' do
+            expect { command_parser['PLACE 5,6,N'] }.to raise_error(Dry::Types::ConstraintError)
+          end
         end
       end
     end
